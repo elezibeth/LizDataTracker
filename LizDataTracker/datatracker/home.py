fb64ad449df562b7d98302311f87d1ebf0867ddc
elezibeth <77396418+elezibeth@users.noreply.github.com>
2021-04-20 17:22:21 -0500

    Preliminary filtering added to json objects returned from API

--- LizDataTracker/datatracker/home.py
+++ LizDataTracker/datatracker/home.py
@@ -5,6 +5,22 @@ from types import SimpleNamespace
 bp = Blueprint('home', __name__)
 
 
+@bp.route('/')
+def index():
+    data = [
+        (1, 1999),
+        (2, 1998),
+        (3, 1957)
+    ]
+    # labels on x axis of chart, values on y axis of chart
+
+    labels = [row[0] for row in data]
+    values = [row[1] for row in data]
+    return render_template(games.html, labels=labels, values=values)
+
+    return '/'
+
+
 @bp.route('/home')
 def index():
     return "liz"
@@ -14,3 +30,29 @@ def index():
 def games():
     response = requests.get('https://api.dccresource.com/api/games').text
     return response
+
+
+@bp.route('/chart')
+def chart():
+    response = requests.get('https://api.dccresource.com/api/games').json()
+    platforms = []
+    for line in response:
+        platform = (x['platform'] for x in response)
+        platforms.append(platform)
+
+    unique_platforms = []
+
+    for x in platforms:
+        if x not in unique_platforms:
+            unique_platforms.append(x)
+
+    quantities = []
+    game_sys_lists = []
+
+    for line in unique_platforms:
+        if game_sys_lists.count() != 0:
+            quantities.append(game_sys_lists)
+        game_sys_lists.clear()
+        for game in response:
+            platform = (x['platform'] for x in response == line)
+            game_sys_lists.append(platform)
